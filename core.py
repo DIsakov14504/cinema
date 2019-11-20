5be4dce610dd02567cebd64189bd48d04e0a0630
DIsakov <55271574+DIsakov14504@users.noreply.github.com>
2019-11-20 18:46:09 +0300

    Update core.py

--- core.py
+++ core.py
@@ -1,1 +1,14 @@
-
+import requests
+from bs4 import BeafutifulSoup
+class CinemaParser:
+  def _init_(self,city):
+    self.city=city
+  def extract_raw_content(self):
+    resp=requests.get(https://msk.subscity.ru)
+    soup=BeautifulSoup(resp.text, 'html.parser')
+    return soup
+  def print_raw_content(self):
+    self.extract_raw_content()
+    return (extract_raw_content(self),prettify())
+  def get_films_list():
+    
