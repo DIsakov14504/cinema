86dab5e3ea60575a359e6032f4f62175789404bb
DIsakov <55271574+DIsakov14504@users.noreply.github.com>
2019-11-22 10:37:39 +0300

    Update core.py

--- cinemaparser/core.py
+++ cinemaparser/core.py
@@ -1,12 +1,26 @@
 import requests
-from bs4 import BeafutifulSoup
+from bs4 import BeautifulSoup
 class CinemaParser:
-  def _init_(self,city):
+  def __init__(self, city='msk'):
+    if city == 'msk':
+      self.url = 'https://msk.subscity.ru'
+    else:
+      self.url = 'https://spb.subscity.ru'
     self.city=city
   def extract_raw_content(self):
-    resp=requests.get(https://msk.subscity.ru)
-    soup=BeautifulSoup(resp.text, 'html.parser')
-    return soup
+    resp=requests.get(self.url)
+    self.content=BeautifulSoup(resp.text, 'html.parser')
   def print_raw_content(self):
     self.extract_raw_content()
-    return (extract_raw_content(self),prettify())
+    print (self.content,prettify())
+  def get_films_list(self):
+    self.extract_raw_content()
+    titles = []
+    films = self.content.find_all('div', {'class': 'movie-plate'})
+    for film in films:
+      titles.append(film["attr-title"])
+    return titles
+msk_parser = CinemaParser()
+print(msk_parser.get_films_list())
+spb_parser = CinemaParser('spb')
+print(spb_parser.get_films_list())
